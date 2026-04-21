33abff50df865f17ea14d558aee788ed8d9894ab
fabskyi <140195814+fabskyi@users.noreply.github.com>
2026-04-21 15:59:24 +0700

    Update home.php

--- home.php
+++ home.php
@@ -77,8 +77,12 @@ if (isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Inventory Management System</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/yanmar.png">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="assets/yanmar.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PT. Yadin Supermarket</title>
 
     <style>
         * {
